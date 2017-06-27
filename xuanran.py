b8d2fcfb4b97235b5a9b646a966d75bafc359ed7
root <yinggang.qiu@net263.com>
2017-06-27 11:25:23 +0800

    渲染模板

---
+++ xuanran.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+	return render_template('index.html')
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html',name=name)
+
+if __name__ == '__main__':
+	app.run(host='211.150.66.48',port=5000,debug=True)
+
