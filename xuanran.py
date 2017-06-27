3db717129e2aa9b6985768ae5a2d244d83f69b1f
root <yinggang.qiu@net263.com>
2017-06-27 11:27:16 +0800

    模板渲染

--- xuanran.py
+++ xuanran.py
@@ -6,7 +6,7 @@ def index():
 	return render_template('index.html')
 @app.route('/user/<name>')
 def user(name):
-	return render_template('user.html',name=name)
+	return render_template('user.html',name=name)    #user.html在templates里
 
 if __name__ == '__main__':
 	app.run(host='211.150.66.48',port=5000,debug=True)
